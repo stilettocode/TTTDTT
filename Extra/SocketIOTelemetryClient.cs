d1527f3ee8f9a1209881aa319bda53e69a1edf76
stilettocode <ianhkim@umich.edu>
2026-04-07 18:37:31 -0400

    changed unity script

--- Extra/SocketIOTelemetryClient.cs
+++ Extra/SocketIOTelemetryClient.cs
@@ -8,28 +8,27 @@ public class SocketIOTelemetryClient : MonoBehaviour
     [Header("Connection")]
     [Tooltip("Socket.IO server URL (e.g., http://localhost:5001)")]
     public string serverUrl = "http://localhost:5001";
-    
+
     private SocketIOClient.SocketIO socket;
     private TelemetryData currentData;
-    
-    // Events
+
     public event Action<TelemetryData> OnTelemetryReceived;
-    
+
     void Start()
     {
         Connect();
     }
-    
+
     void OnDestroy()
     {
         Disconnect();
     }
-    
+
     void OnApplicationQuit()
     {
         Disconnect();
     }
-    
+
     private async void Connect()
     {
         try
@@ -40,28 +39,31 @@ public class SocketIOTelemetryClient : MonoBehaviour
                 ReconnectionDelay = 1000,
                 ReconnectionAttempts = 5
             });
-            
+
             socket.OnConnected += (sender, e) =>
             {
                 Debug.Log("[SocketIO] Connected");
             };
-            
+
             socket.OnDisconnected += (sender, e) =>
             {
                 Debug.Log("[SocketIO] Disconnected");
             };
-            
-            socket.On("telemetry", response =>
+
+            socket.On("eva-telemetry", response =>
             {
                 try
                 {
                     string json = response.GetValue<string>();
                     currentData = JsonConvert.DeserializeObject<TelemetryData>(json);
-                    
-                    if (currentData != null)
+
+                    if (currentData?.telemetry != null)
                     {
                         OnTelemetryReceived?.Invoke(currentData);
-                        Debug.Log($"[SocketIO] Received telemetry - EVA Time: {currentData.telemetry.eva_time}");
+                        var e1 = currentData.telemetry.eva1;
+                        var e2 = currentData.telemetry.eva2;
+                        Debug.Log(
+                            $"[SocketIO] eva-telemetry eva1 elapsed={e1?.eva_elapsed_time ?? -1f} eva2 elapsed={e2?.eva_elapsed_time ?? -1f}");
                     }
                 }
                 catch (Exception ex)
@@ -69,13 +71,23 @@ public class SocketIOTelemetryClient : MonoBehaviour
                     Debug.LogError($"[SocketIO] Parse error: {ex.Message}");
                 }
             });
-            
+
             socket.On("error", response =>
             {
-                var errorData = response.GetValue<ErrorData>();
-                Debug.LogError($"[SocketIO] Error: {errorData.error}");
+                try
+                {
+                    var errorData = response.GetValue<ErrorData>();
+                    if (errorData != null)
+                        Debug.LogError($"[SocketIO] Error: {errorData.error}");
+                    else
+                        Debug.LogError("[SocketIO] Error (no payload)");
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError($"[SocketIO] Error event parse: {ex.Message}");
+                }
             });
-            
+
             await socket.ConnectAsync();
         }
         catch (Exception ex)
@@ -83,7 +95,7 @@ public class SocketIOTelemetryClient : MonoBehaviour
             Debug.LogError($"[SocketIO] Connection error: {ex.Message}");
         }
     }
-    
+
     private async void Disconnect()
     {
         if (socket != null)
@@ -93,7 +105,7 @@ public class SocketIOTelemetryClient : MonoBehaviour
             socket = null;
         }
     }
-    
+
     public TelemetryData GetCurrentData()
     {
         return currentData;
@@ -104,12 +116,17 @@ public class SocketIOTelemetryClient : MonoBehaviour
 public class TelemetryData
 {
     public TelemetryInfo telemetry;
+    public StatusData status;
+    public DcuData dcu;
+    public EvaTelemetryError error;
+    public ImuData imu;
+    public UiaData uia;
+    public string local_timestamp;
 }
 
 [Serializable]
 public class TelemetryInfo
 {
-    public float eva_time;
     public EvaData eva1;
     public EvaData eva2;
 }
@@ -117,28 +134,110 @@ public class TelemetryInfo
 [Serializable]
 public class EvaData
 {
-    public float batt_time_left;
+    public float primary_battery_level;
+    public float secondary_battery_level;
+    public float battery_level;
     public float oxy_pri_storage;
     public float oxy_sec_storage;
     public float oxy_pri_pressure;
     public float oxy_sec_pressure;
-    public float oxy_time_left;
-    public float heart_rate;
-    public float oxy_consumption;
-    public float co2_production;
     public float suit_pressure_oxy;
     public float suit_pressure_co2;
     public float suit_pressure_other;
     public float suit_pressure_total;
+    public float helmet_pressure_co2;
     public float fan_pri_rpm;
     public float fan_sec_rpm;
-    public float helmet_pressure_co2;
     public float scrubber_a_co2_storage;
     public float scrubber_b_co2_storage;
     public float temperature;
-    public float coolant_ml;
+    public float coolant_storage;
     public float coolant_gas_pressure;
     public float coolant_liquid_pressure;
+    public float heart_rate;
+    public float oxy_consumption;
+    public float co2_production;
+    public float eva_elapsed_time;
+}
+
+[Serializable]
+public class StatusData
+{
+    public bool started;
+}
+
+[Serializable]
+public class DcuData
+{
+    public DcuEva1 eva1;
+    public DcuEva2 eva2;
+}
+
+[Serializable]
+public class DcuEva1
+{
+    public bool oxy;
+    public bool fan;
+    public bool pump;
+    public bool co2;
+    public DcuEva1Batt batt;
+}
+
+[Serializable]
+public class DcuEva1Batt
+{
+    public bool lu;
+    public bool ps;
+}
+
+[Serializable]
+public class DcuEva2
+{
+    public bool batt;
+    public bool oxy;
+    public bool comm;
+    public bool fan;
+    public bool pump;
+    public bool co2;
+}
+
+[Serializable]
+public class EvaTelemetryError
+{
+    public bool fan_error;
+    public bool oxy_error;
+    public bool power_error;
+    public bool scrubber_error;
+}
+
+[Serializable]
+public class ImuData
+{
+    public ImuEva eva1;
+    public ImuEva eva2;
+}
+
+[Serializable]
+public class ImuEva
+{
+    public float posx;
+    public float posy;
+    public float heading;
+}
+
+[Serializable]
+public class UiaData
+{
+    public bool eva1_power;
+    public bool eva1_oxy;
+    public bool eva1_water_supply;
+    public bool eva1_water_waste;
+    public bool eva2_power;
+    public bool eva2_oxy;
+    public bool eva2_water_supply;
+    public bool eva2_water_waste;
+    public bool oxy_vent;
+    public bool depress;
 }
 
 [Serializable]
@@ -147,4 +246,3 @@ public class ErrorData
     public string error;
     public string local_timestamp;
 }
-
